25b54eb367a50be72ad1aaaaabcdd004e083633b
spaceninja51 <cwg2423@gmail.com>
2024-09-09 19:51:54 -0400

    no more hard coded API key :D

--- apodv2.py
+++ apodv2.py
@@ -6,43 +6,37 @@ import json
 import requests
 # For checking file existance by name
 from datetime import datetime
-
 # Input url and request keys, matching the dict key to the
 # api url segment
 url = "https://api.nasa.gov/planetary/apod?"
-
-location = os.getcwd()
-
 keys = dict(
     api_key = "",
 )
-
+# Reads the API key from a text file named api_key.txt in 
+# a folder named secrets
+with open("Astropic/secrets/api_key.txt",'rt') as key:
+    keys["api_key"] = key.read()
 # change to data folder, if its not there, make it
+location = os.getcwd()
 try:
-    os.chdir(location+"/data")
+    os.chdir(location+"/Astropic/data")
 except:
-    os.mkdir(location+"/data")
-    os.chdir(location+"/data")
-
-date = datetime.today().strftime('%Y-%m-%d')
-
+    os.mkdir(location+"/Astropic/data")
+    os.chdir(location+"/Astropic/data")
 # Only make an API call if the data hasn't already been collected
+date = datetime.today().strftime('%Y-%m-%d')
 if not os.path.isfile(date+".json"):
-    
     # Initial call, returns a Response object from the given url.
     # If keys are provided, builds them into the url
     call = requests.get(url,keys)
-    
     # Returns the API response's headers as a dict
     # The headers property of a request is a dict by default
     headers = call.headers
-    
     # Converts the JSON response into a dict
     # Takes the text of the response and turns it into a dict
     response = json.loads(call.text)
     allowance = {"Remaining Calls":headers._store['x-ratelimit-remaining'][1]}
     response.update(allowance)
-    
     # Writes the json response to a file named using the date
     # of the response
     with open(date+".json",'w') as pad:
@@ -50,17 +44,14 @@ if not os.path.isfile(date+".json"):
 else:
     with open(date+".json",'r') as data:
         response = json.loads(data.read())
-
-# change to photo data folder, if its not there, make it
+# change to photo subfolder, if its not there, make it
 try:
     os.chdir("./photos")
 except:
     os.mkdir("./photos")
     os.chdir("./photos")
-
 # Only check for/download the image if it hasn't been downloaded
 if not os.path.isfile(response["title"]+".jpg"):
-    
     # Uses the response's image URL to save the photo from the
     # byte formatted answer
     photo = requests.get(response["url"]).content
