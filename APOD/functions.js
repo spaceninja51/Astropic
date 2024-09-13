6be611499450ac7ec4050e51603d6306084cc866
spaceninja51 <cwg2423@gmail.com>
2024-09-12 21:54:39 -0400

    was not ready for HTML

--- APOD/functions.js
+++
@@ -1,6 +0,0 @@
-function retrieve_pic() {
-    const reponse = fetch("Astropic/data/2024-09-09.json")
-    const data = response.json()
-    current = data.url
-    document.getElementById(today).src=current
-}
